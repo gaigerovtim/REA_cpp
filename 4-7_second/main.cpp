6e4f26f9ff54c866aaef652dbf64c3f54ed71767
gaigerovtim <1501tima@gmail.com>
2018-05-09 13:47:26 +0300

    Добавлено заполнение структуры и вывод ее на экран

--- 4-7_second/main.cpp
+++ 4-7_second/main.cpp
@@ -3,17 +3,40 @@
 using namespace std;
 
 struct hachi {
-    string name[80];
+    string name;
     long int artical;
     int count;
     double price;
     unsigned short int sale;
 };
 int main() {
-    struct hachi pdr;
+    setlocale(LC_ALL, "russian");
+    int col; //Вводим колличетво позиций
+    cout << "Введите колличество позиций: ";
+    cin >> col;
+    struct hachi pdr;  //Выделяем память под структуру
     hachi *pdr1 = new hachi;
     FILE *BD = fopen ("RUSSIA_SILA_KAVKAZCI_GNIDU.dat", "wb");  // Это первый пункт, в ВС писать fopen_s
-    cin << name << "%n" << artical << "%n" << count << "%n" << price << "%n" << sale << endl;
-    fclose(BD);
+    hachi about[col];
     
+    // ЗАПОЛНЕНИЕ СТРУКТУРЫ
+    for (int i = 0; i < col; i++){
+        cout << "Наименование: ";
+        cin >> about[ i ].name;
+        cout << "Артикул: ";
+        cin >> about[ i ].artical;
+        cout << "Колличество: ";
+        cin >> about[ i ].count;
+        cout << "Цена: ";
+        cin >> about[ i ].price;
+        cout << "Скидка: ";
+        cin >> about[ i ].sale;
+    }
+    
+    //ВЫВОД НА ЭКРАН В ВИДЕ
+    //Артикул Наименование Цена Количество Скидка
+    for (int j = 0; j < col; j++) {
+        cout << about[ j ].name << " | " << about[ j ].artical << " | " << about[ j ].count << " | " << about[ j ].price << " | " << about[ j ].sale << endl;
+    }
+    fclose(BD); // Закрытие БД
 }

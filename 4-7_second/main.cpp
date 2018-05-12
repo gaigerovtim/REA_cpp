6911182468838e3d0495bd73dce3dfe9ea4398f6
gaigerovtim <1501tima@gmail.com>
2018-05-12 17:39:10 +0300

    Прописано нормальное миню

--- 4-7_second/main.cpp
+++ 4-7_second/main.cpp
@@ -1,65 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <fstream>
 #include <iostream>
 using namespace std;
 
-//СТРУКТУРА
-struct hachi {
-    string name;
-    long int artical;
-    int count;
-    double price;
-    unsigned short int sale;
-};
+typedef struct good {
+    string name; // имя
+    long int artical; // артикул
+    int count; // количество
+    double price; // цена
+    unsigned short int sale; // скидка
+} good;
 
-void print_menu_first() {
-    system("cls");  // очищаем экран
-    cout << "\nСоздать БД\n" << endl;
-    cout << "\nЗагрузить данные\n" << endl;
-    cout << ">>>";
-}
-void print_menu_second() {
-    system("cls");  // очищаем экран
-    cout << "\nСоздать БД\n" << endl;
-    cout << "\nВывести все позиции в формате \n" << endl;
-    cout << "\nВыход\n" << endl;
-    /*cout << "Добавить позицию\n" << endl;
-     cout << "Поиск позиции по артикулу" << endl;
-     cout << "Вывести все позиции со скидкой" << endl;
-     cout << "Вывести все позиции, которых нет в наличии" << endl;
-     cout << "Отсортировать все позиции по указанному полю" << endl;
-     cout << "Сохранить БД\n" << endl;*/
-    cout << ">>>";
-}
-
-//Создание структуры для меню
-/* int menu0() {
- int variant0;
- cout << "\nСоздать БД\n" << endl;
- cout << "\nЗагрузить данные\n" << endl;
- cout << ">>>";
- cin >> variant0;
- return variant0;
- }
- 
- //Создание структуры для меню
- int menu1() {
- int variant1;
- cout << "\nСоздать БД\n" << endl;
- cout << "\nВывести все позиции в формате \n" << endl;
- cout << "\nВыход\n" << endl;
- cout << "Добавить позицию\n" << endl;
- cout << "Поиск позиции по артикулу" << endl;
- cout << "Вывести все позиции со скидкой" << endl;
- cout << "Вывести все позиции, которых нет в наличии" << endl;
- cout << "Отсортировать все позиции по указанному полю" << endl;
- cout << "Сохранить БД\n" << endl;
- cout << ">>>";
- cin >> variant1;
- return variant1;
- } */
-
-//Создание новой БД
-void array_new_bd ( int n, hachi about[])
+// Создание новой БД
+void array_new_bd ( int n, good about[])
 {
     for (int i = 0; i < n; i++){
         cout << "Наименование: ";
@@ -75,8 +29,8 @@ void array_new_bd ( int n, hachi about[])
     }
 };
 
-//Поиск по артиклю
-void array_find_artical ( int l, int n, hachi about[])
+// Поиск по артиклю
+void array_find_artical ( int l, int n, good about[])
 {
     cout << "Введите артикул";
     cin >> l;
@@ -84,23 +38,23 @@ void array_find_artical ( int l, int n, hachi about[])
         if (about[ s ].artical == l)
         {
             cout << about[ s ].name << " | " << about[ s ].artical << " | " << about[ s ].count << " | " << about[ s ].price << " | " << about[ s ].sale << endl;
-            //ДОПИЛИТЬ ПЕРЕЗАПИСЬ ДАННЫХ, ПОСЛЕ ТОГО КАК ДАНЯ ПРИШЛЕТ КНОПКИ
-            //УДАЛЕНИЕ ПОЗИЦИЙ
+            // ДОПИЛИТЬ ПЕРЕЗАПИСЬ ДАННЫХ, ПОСЛЕ ТОГО КАК ДАНЯ ПРИШЛЕТ КНОПКИ
+            // УДАЛЕНИЕ ПОЗИЦИЙ
         }
     }
 };
 
-//Вывод на экран в фотмате
-//Наименование Артикль Количество Цена Скидка
-void array_print ( int n, hachi about[])
+// Вывод на экран в фотмате
+// Наименование Артикль Количество Цена Скидка
+void array_print ( int n, good about[])
 {
     for (int j = 0; j < n; j++) {
         cout << about[ j ].name << " | " << about[ j ].artical << " | " << about[ j ].count << " | " << about[ j ].price << " | " << about[ j ].sale << endl;
     }
 };
 
-//Вывод товаров со скидкой
-void array_sale ( int n, hachi about[])
+// Вывод товаров со скидкой
+void array_sale ( int n, good about[])
 {
     for (int r = 0; r < n; r++) {
         if (about[ r ].sale > 0)
@@ -110,8 +64,8 @@ void array_sale ( int n, hachi about[])
     }
 };
 
-//Вывод товаров, которых нет в наличии
-void array_count ( int n, hachi about[])
+// Вывод товаров, которых нет в наличии
+void array_count ( int n, good about[])
 {
     for (int l = 0; l < n; l++) {
         if (about[ l ].count == 0)
@@ -121,99 +75,109 @@ void array_count ( int n, hachi about[])
     }
 };
 
+void print_menu() {
+    printf("1. Создать БД\n");
+    printf("2. Вывести все позиции\n");
+    printf("3. Добавить позицию\n");
+    printf("4. Поиск позиции по артикулу\n");
+    printf("5. Вывести все позиции со скидкой\n");
+    printf("6. Вывести все позиции, которых нет в наличии\n");
+    printf("7. Отсортировать все позиции по указанному полю\n");
+    printf("8. Сохранить БД\n");
+    printf("9. Выход\n");
+    printf(">");
+}
+
+void print_main_menu() {
+    printf("1. Создать БД\n");
+    printf("2. Загрузить данные\n");
+    printf("3. Выход\n");
+    printf(">");
+}
+
 int get_variant(int count) {
     int variant;
-    string s; // строка для считывания введённых данных
-    getline(cin, s); // считываем строку
-    
+    char s[100]; // строка для считывания введённых данных
+    scanf("%s", s); // считываем строку
     // пока ввод некорректен, сообщаем об этом и просим повторить его
-    while (sscanf(s.c_str(), "%d", &variant) != 1 || variant < 1 || variant > count) {
-        cout << "Incorrect input. Try again: "; // выводим сообщение об ошибке
-        getline(cin, s); // считываем строку повторно
+    while (sscanf(s, "%d", &variant) != 1 || variant < 1 || variant > count) {
+        printf("Некорректный ввод. Попробуйте еще раз: "); // выводим сообщение об ошибке
+        scanf("%s", s); // считываем строку повторно
     }
-    
     return variant;
 }
 
-
 int main() {
-    setlocale(LC_ALL, "russian");
-    int col, variant;
-    long int find_artical;
-    struct hachi pdr;
-    hachi *pdr1 = new hachi;
-    hachi about[col];
-    
-    do {
-        print_menu_first(); // выводим меню на экран
-        
-        variant = get_variant(5); // получаем номер выбранного пункта меню
-        
-        switch (variant) {
-            case 1:
-                
-                break;
-                
-            case 2:
-                
-                break;
-                
-            case 3:
-                
-                break;
-                
-            case 4:
-                
-                break;
-        }
-        
-        if (variant != 5)
-            system("pause"); // задерживаем выполнение, чтобы пользователь мог увидеть результат выполнения выбранного пункта
-    } while (variant != 5);
-    
-    
-    //MENU-0
     FILE *base;
-    /*   int variant0 = menu0();
-     switch (variant0)
-     {
-     case 1:
-     base = fopen("/Users/andrvdnl/Desktop/base.dat", "wb");
-     cout << "Введите колличество позиций: ";
-     cin >> col;
-     array_new_bd(col, about);
-     fclose(base);
-     break;
-     
-     case 2:
-     break;
-     }
-     
-     //MENU-1
-     int variant1 = menu1();
-     bool ok = false;
-     while (ok != true) {
-     switch (variant1)
-     {
-     case 1:
-     base = fopen("base.dat", "wb");
-     cout << "Введите колличество позиций: ";
-     cin >> col;
-     array_new_bd(col, about);
-     fclose(base);
-     break;
-     
-     case 2:
-     array_print(col, about);
-     break;
-     
-     case 3:
-     ok = true;
-     break;
-     }
-     }
-     array_find_artical(find_artical, col, about);
-     array_sale(col, about);
-     array_count(col, about);
-     */
+    setlocale(LC_ALL, "russian");
+    int variant; // выбранный пункт меню
+    int variant_0; // выбранный пункт меню
+    int capacity = 1;
+    int col;
+    struct good pdr;
+    good *pdr1 = new good;
+    good about[col];
+    good *goods = (good *)malloc(capacity * sizeof(good)); // выделяем память под массив
+    
+    // ПЕРВОЕ МЕНЮ
+    print_main_menu();
+    variant_0 = get_variant(3);
+    switch (variant_0)
+    {
+        case 1:
+            base = fopen("/Users/andrvdnl/Desktop/base.dat", "wb");
+            cout << "Введите колличество позиций: ";
+            cin >> col;
+            array_new_bd(col, about);
+            fclose(base);
+            break;
+            
+        case 2:
+            break;
+    }
+    
+    // ОСНОВНОЕ МЕНЮ
+    if (variant_0 != 3) { // чтобы работала кнопка выхода в первом меню
+        do {
+            print_menu(); // выводим меню на экран
+            
+            variant = get_variant(9); // получаем номер выбранного пункта меню
+            
+            switch (variant) {
+                case 1:
+                    base = fopen("/Users/andrvdnl/Desktop/base.dat", "wb");
+                    cout << "Введите колличество позиций: ";
+                    cin >> col;
+                    array_new_bd(col, about);
+                    fclose(base);
+                    break;
+                    
+                case 2:
+                    array_print(col, about);
+                    break;
+                    
+                case 3:
+                    break;
+                    
+                case 4:
+                    break;
+                    
+                case 5:
+                    break;
+                    
+                case 6:
+                    break;
+                    
+                case 7:
+                    break;
+                    
+                case 8:
+                    break;
+            }
+            
+            if (variant != 9)
+                system("pause"); // задерживаем выполнение, чтобы пользователь мог увидеть результат выполнения выбранного пункта
+        } while (variant != 9);
+    }
+    return 0;
 }
